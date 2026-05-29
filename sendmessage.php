78be9de07c8bc0c0a0cc55770d80a77105ddd9ab
isuzubali <karyateknik2024@gmail.com>
2026-05-29 20:03:21 +0800

    Update sendmessage.php

--- sendmessage.php
+++ sendmessage.php
@@ -1,1 +1,35 @@
+<?php
+function Kirimfonnte($data, $telp)
+{
+$curl = curl_init();
 
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://api.fonnte.com/send',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS => array(
+'target' => $telp,
+'message' => $data, 
+'countryCode' => '62', //optional
+),
+  CURLOPT_HTTPHEADER => array(
+    'Authorization: 4KY7WmJqDweDDLsNRFep' //change TOKEN to your actual token
+  ),
+));
+}
+Kirimfonnte($token, $data);
+$response = curl_exec($curl);
+if (curl_errno($curl)) {
+  $error_msg = curl_error($curl);
+}
+curl_close($curl);
+
+if (isset($error_msg)) {
+ echo $error_msg;
+}
+echo $response;
