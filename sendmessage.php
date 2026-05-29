1d4ef5dc9032422edc6012c0649b6c3820efa633
isuzubali <karyateknik2024@gmail.com>
2026-05-29 20:33:25 +0800

    Update sendmessage.php

--- sendmessage.php
+++ sendmessage.php
@@ -1,26 +1,25 @@
 <?php
+$token = "4KY7WmJqDweDDLsNRFep";
 function Kirimfonnte($data, $telp)
 {
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://api.fonnte.com/send',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS => array(
-'target' => $telp,
-'message' => $data, 
-'countryCode' => '62', //optional
-),
-  CURLOPT_HTTPHEADER => array(
-    'Authorization: 4KY7WmJqDweDDLsNRFep' //change TOKEN to your actual token
-  ),
-));
+        CURLOPT_URL => 'https://api.fonnte.com/send',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => array(
+            'target' => $telp,
+            'message' => $data,
+        ),
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: ' . $token
+        ),));
 
 $response = curl_exec($curl);
 if (curl_errno($curl)) {
